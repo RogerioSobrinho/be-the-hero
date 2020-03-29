8a62e56edfec29773ffcf203e6483a0a599943b5
rogerio.sobrinho <x216948>
2020-03-29 18:49:40 -0300

    mobile app

---
+++ mobile/src/routes.js
@@ -0,0 +1,22 @@
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+const AppStack = createStackNavigator();
+import React from 'react';
+
+import Incidents from './pages/Incidents';
+import Detail from './pages/Detail';
+
+export default function Routes() {
+  return (
+    <NavigationContainer>
+      <AppStack.Navigator
+        screenOptions={{
+          headerShown: false, // Disable Header
+        }}
+      >
+        <AppStack.Screen name="Incidents" component={Incidents} />
+        <AppStack.Screen name="Detail" component={Detail} />
+      </AppStack.Navigator>
+    </NavigationContainer>
+  );
+}
